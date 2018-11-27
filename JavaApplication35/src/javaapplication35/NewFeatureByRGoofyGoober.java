aa06b5d481ecb9a0a46d120fea34a41163955bc6
muham <muham@DESKTOP-UDNBJHB>
2018-11-27 08:06:29 +0700

    Coba buat fitur baru

---
+++ JavaApplication35/src/javaapplication35/NewFeatureByRGoofyGoober.java
@@ -0,0 +1,7 @@
+package javaapplication35;
+
+public class NewFeatureByRGoofyGoober {
+    public static void main(String[] args) {
+        System.out.println("A New Feature By RGoofyGoober");
+    }
+}
