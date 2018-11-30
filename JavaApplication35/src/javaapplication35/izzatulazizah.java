543f0183b629603573ea6cf164e4c719c367799d
Izza <Izza@Izza-PC>
2018-11-30 09:25:40 +0800

    hanya tentang perkalian sederhana

---
+++ JavaApplication35/src/javaapplication35/izzatulazizah.java
@@ -0,0 +1,11 @@
+
+package javaapplication35;
+
+public class izzatulazizah {
+    public static void main(String[] args) {
+        int a=9;
+        int b=67;
+        int z=a*b;
+        System.out.println(z);
+    }
+}
